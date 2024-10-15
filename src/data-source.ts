d086bd8137fdb289172f3548664b625b8015f219
Adrienne Rio Wongso Atmojo <aatm0001@student.monash.edu>
2024-10-16 01:53:55 +0800

    chore: backup stuff for tokens

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,15 +1,16 @@
 import { DataSource } from 'typeorm';
+import { Token } from './models';
 
 export const AppDataSource = new DataSource({
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'JkP%7NqgGGj8TGJz',
-  database: 'postgres',
-  synchronize: true,
-  logging: true,
-  entities: [],
-  subscribers: [],
-  migrations: [],
+    type: 'postgres',
+    host: 'localhost',
+    port: 5431,
+    username: 'postgres',
+    password: 'JkP%7NqgGGj8TGJz',
+    database: 'postgres',
+    synchronize: true,
+    logging: true,
+    entities: [Token],
+    subscribers: [],
+    migrations: [],
 });
--- src/index.ts
+++ src/index.ts
@@ -5,18 +5,32 @@ import express, { Express } from 'express';
 import helmet from 'helmet';
 
 import { AppDataSource } from './data-source';
+import { TokenRouter } from '@/routers';
 
-const app: Express = express();
-app.use(cors());
-// parse various different custom JSON types as JSON
-app.use(bodyParser.json());
-// Set the application to trust the reverse proxy
-app.set('trust proxy', true);
-// Middlewares
-app.use(helmet());
+async function main() {
+    const app: Express = express();
+    app.use(cors());
+    // parse various different custom JSON types as JSON
+    app.use(bodyParser.json());
+    // Set the application to trust the reverse proxy
+    app.set('trust proxy', true);
+    // Middlewares
+    app.use(helmet());
 
+    const dataSource = await AppDataSource.initialize();
+    const tokenRouter = new TokenRouter(dataSource);
 
-AppDataSource.initialize().catch((error) => console.log(error));
-const server = app.listen(8002, () => {
-  console.log('server started')
-});
+    app.use('/api/tokens', tokenRouter.router);
+
+    const server = app.listen(8002, () => {
+        console.log('server started');
+    });
+}
+
+(async () => {
+    try {
+        await main();
+    } catch (e) {
+        console.log('error', e);
+    }
+})();
