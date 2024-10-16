e8cf92e57fcb65550708f622c4c8caa4c4cb0735
Jim Daniel Wasswa <jimdanielswasswa@outlook.com>
2024-10-16 19:46:57 +0800

    chore: attach entities to the data source

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,15 +1,20 @@
 import { DataSource } from 'typeorm';
+import { Link } from './models/link';
+import { TokenAccount } from './models/tokenAccount';
+import { Transaction } from './models/transaction';
+import { Wallet } from './models/wallet';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
   host: 'localhost',
   port: 5432,
   username: 'postgres',
+  // TODO: rotate the password and move it to env
   password: 'JkP%7NqgGGj8TGJz',
   database: 'postgres',
   synchronize: true,
   logging: true,
-  entities: [],
+  entities: [Link, TokenAccount, Transaction, Wallet],
   subscribers: [],
   migrations: [],
 });
