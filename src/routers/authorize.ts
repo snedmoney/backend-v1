14a25c560e77ee43ed2c0b85c2a423e0a48186db
henry-hein <henryhein.hein@gmail.com>
2024-10-22 08:59:55 -0700

    update auth

--- src/routers/authorize.ts
+++ src/routers/authorize.ts
@@ -1,39 +1,90 @@
+import { Request, Response, Router } from 'express';
+
+import { DataSource } from 'typeorm';
+// import { Setting } from '@/models/setting';
+import { User } from '@/models';
+// import { UserRole, Wallet } from '@/models/wallet';
+import { WalletService } from '@/services/wallet';
 import { createToken } from '@/util/jwt';
-import { Router, Request } from 'express';
+// import { getDataSource } from '@/data-source';
 import { recoverMessageAddress } from 'viem';
-import { AppDataSource } from '@/data-source';
-import { Wallet, UserRole } from '@/models_dep/wallet';
-import { Setting } from '@/models_dep/setting';
 
-const router = Router();
+// const router = Router();
 
-type AuthRequest = { signature: `0x${string}`; role: UserRole };
+// type AuthRequest = { signature: `0x${string}`; role: UserRole };
 
-router.post('/', async (req: Request<any, any, AuthRequest>, res) => {
-  const walletRepository = AppDataSource.getRepository(Wallet);
-  const settingRepository = AppDataSource.getRepository(Setting);
-  const { signature, role } = req.body;
-  try {
-    const address = await recoverMessageAddress({
-      message: 'hello world',
-      signature: signature,
-    });
-    const wallet = walletRepository.find({ where: { walletAddress: address } });
-    if (!wallet) {
-      const newWallet = walletRepository.create({ walletAddress: address });
-      
-      if (role === UserRole.CREATOR) {
-        newWallet.role = UserRole.CREATOR;
-        const setting = settingRepository.create();
-        newWallet.setting = setting;
-      }
-      await walletRepository.save(newWallet);
+// router.post('/', async (req: Request<any, any, AuthRequest>, res) => {
+//     const AppDataSource = await getDataSource();
+//     const walletRepository = AppDataSource.getRepository(Wallet);
+//     const settingRepository = AppDataSource.getRepository(Setting);
+//     const { signature, role } = req.body;
+//     try {
+//         const address = await recoverMessageAddress({
+//             message: 'hello world',
+//             signature: signature,
+//         });
+//         const wallet = walletRepository.find({
+//             where: { walletAddress: address },
+//         });
+//         if (!wallet) {
+//             const newWallet = walletRepository.create({
+//                 walletAddress: address,
+//             });
+
+//             if (role === UserRole.CREATOR) {
+//                 newWallet.role = UserRole.CREATOR;
+//                 const setting = settingRepository.create();
+//                 newWallet.setting = setting;
+//             }
+//             await walletRepository.save(newWallet);
+//         }
+//         const token = createToken({ address });
+//         res.json({ token });
+//     } catch (e) {
+//         res.status(400).json({ error: 'Bad Request' });
+//     }
+// });
+
+// export default router;
+
+type AuthRequest = { signature: `0x${string}`; };
+
+export class AuthRoutes {
+  
+    walletService: WalletService;
+    router: Router;
+    constructor(dataSource: DataSource) {
+        this.router = Router();
+        this.walletService = new WalletService(dataSource);
+        this.registerRoutes();
     }
-    const token = createToken({ address });
-    res.json({ token });
-  } catch (e) {
-    res.status(400).json({ error: 'Bad Request' });
-  }
-});
+    registerRoutes() {
+        this.router.post('/', this.authorize);
+    }
 
-export default router;
+    authorize = async (req: Request<any, any, AuthRequest>, res: Response) => {
+        const { signature } = req.body;
+        if (!signature) {
+            return res.status(400).json({
+                error: 'Signature missing',
+            });
+        }
+        try {
+            const address = await recoverMessageAddress({
+                message: 'hello world',
+                signature: signature,
+            });
+            const wallet = await this.walletService.getWalletByAddress(address);
+            if (!wallet) {
+                await this.walletService.createWallet({
+                    address,
+                    user: new User(), // TODO: Verify how the user entity fits in.
+                });
+            }
+            const token = createToken({ address });
+            res.json({ token });
+        } catch (e) {
+            res.status(400).json({ error: 'Bad Request' });
+        }
+    };
+}
